6aa446ca2a8514c35c43ee9d0554b6e8d6cb0d95
honghe <leopardsaga@gmail.com>
2012-12-08 22:40:21 +0800

    first prototype

--- org/honghe/hello/text.py
+++ org/honghe/hello/text.py
@@ -7,7 +7,6 @@
     1. 下载mp3文件模块未实现，所以先以wget小工具顶上
     2. 在问题1的情况下，保存的bat文件是UFT-8编码，其运行时处理非英文字符时有问题
 '''
-import urllib
 
 __author__ = 'honghe'
 
@@ -40,7 +39,7 @@ def fetch():
 #    保存成简单bat文件
     with open("%s/down.bat" % PREFIX, 'w') as down:
 #        只选几个页面做测试
-        for i in xrange(395, 396):
+        for i in xrange(394, 396):
             url = 'http://www.luoo.net/radio/radio%s/mp3.xml' % i
             r = requests.get(url)
             if r.status_code == 200:
@@ -48,7 +47,7 @@ def fetch():
                     extract('path="', '"', r.content),
                     extract('title="', '"', r.content)
                 ):
-                    urllib.urlretrieve(path, )
+                    down.write('wget %s -O "%s/%s.mp3"\n' % (path, PREFIX, name))
 
 
 if __name__ == '__main__':
